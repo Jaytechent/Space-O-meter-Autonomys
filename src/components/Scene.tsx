0f8149111407d0ae4b5d331f3999e5b4b3173adc
jaytechent <fasasiolamilekan35@gmail.com>
2024-12-03 13:48:15 +0100

    update dep

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -4,8 +4,8 @@ import { NetworkId } from "@autonomys/auto-utils";
 import { useCallback, useEffect, useState } from "react";
 import Footer from "../app/Footer";
 
-const MAX_SPACE = 10000;
-const MAX_SPACEG = 1000;
+const MAX_SPACE = 100;
+const MAX_SPACEG = 10000;
 
 const calculatePercentage = (value: number, max: number) => (value / max) * 100;
 
@@ -24,8 +24,10 @@ export const Scene = () => {
       ]);
       console.log("Taurus Data:", taurus);
       console.log("Gemini Data:", gemini);
-      // setTaurusData(taurus);
-      // setGeminiData(gemini);
+
+      // Update state with the fetched data
+      setTaurusData(taurus);
+      setGeminiData(gemini);
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
